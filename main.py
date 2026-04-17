dada5c4c7c817f5f5466b67c098514d9bf9540b0
Soham Tambde <65997073+Sohamtam46@users.noreply.github.com>
2026-04-17 15:02:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import requests
 from twilio.rest import Client
 
 MY_EMAIL = os.environ.get("MY_EMAIL")
+TO_EMAIL = os.environ.get("TO_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-def send_email():
+def send_email(message):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
         connection.sendmail(
                             from_addr=MY_EMAIL,
-                            to_addrs=MY_EMAIL,
-                            msg=f"Subject:Weather Update!\n\nIts Gonna rain. Carry an Umbrella!"
+                            to_addrs=TO_EMAIL,
+                            msg=f"Subject:😺Weather Update!\n\n{message}"
                             )
 
 OPEN_WEATHER_API_KEY = os.environ.get("OWM_API_KEY")
@@ -30,6 +31,9 @@ response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", 
 response.raise_for_status()
 data = response.json()
 
+RAINY_WEATHER_MESSAGE = "Its Gonna rain🌧️. Carry an Umbrella☔!"
+CLEAR_WEATHER_MESSAGE = "It's Sunny☀️ Outside! Got to the beach🏖️"
+
 will_rain = False
 weather_codes = [int(data_point["weather"][0]["id"]) for data_point in data["list"] if int(data_point["weather"][0]["id"]) < 700]
 for codes in weather_codes:
@@ -37,5 +41,9 @@ for codes in weather_codes:
         will_rain = True
 
 if will_rain:
-    send_email()
+    send_email(RAINY_WEATHER_MESSAGE)
+else:
+    send_email(CLEAR_WEATHER_MESSAGE)
+
+    
 
